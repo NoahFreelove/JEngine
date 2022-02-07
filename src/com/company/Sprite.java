5f87bc8e5cfae66858e31f0b411cc5176f106ff4
Noah Freelove <90488068+NoahFreelove@users.noreply.github.com>
2022-02-07 15:59:06 -0500

    added getSprite and constructor

--- src/com/company/Sprite.java
+++ src/com/company/Sprite.java
@@ -7,9 +7,15 @@ import java.awt.*;
  * A object cannot be controlled by the player, but it's position can be updated
  * **/
 public class Sprite {
-    Object object;
-    Image sprite;
+    public Object object;
+    private Image sprite;
 
+    public Image getSprite() {return sprite;}
     public void setSprite(Image newSprite) {sprite = newSprite;}
     public void migrateObject(Object newObject) {object = newObject;}
+    
+    public Sprite(Transform transform, Image newSprite) {
+        object.Transform = transform;
+        setSprite(newSprite);
+    }
 }
