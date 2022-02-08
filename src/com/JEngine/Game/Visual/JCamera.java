709fc773d8973fa4547660b642b30ded2af7c700
Noah Freelove <90488068+NoahFreelove@users.noreply.github.com>
2022-02-08 11:18:44 -0500

    Added support for camera parent

--- src/com/JEngine/Game/Visual/JCamera.java
+++ src/com/JEngine/Game/Visual/JCamera.java
@@ -10,14 +10,25 @@ import java.awt.*;
 public class JCamera extends Object{
     public JScene scene;
     public JWindow window;
-
-    public JCamera(JWindow window, JScene scene) {
+    public Object parent;
+    
+    public JCamera(JWindow window, JScene scene, Object parent) {
         this.window = window;
         this.scene = scene;
+        if(parent != null)
+        {
+            this.parent = parent
+        }
     }
 
+    public void GetObjectsInView()
+    {
+        //super.transform.position = parent.transform.position
+    }
+    
     public void Render()
     {
+        
         JPanel panel = (JPanel) window.frame.getContentPane();
         JLabel jl = new JLabel(new JImage(true, "bin\\\\image.png").getImage());
         panel.add(jl);
